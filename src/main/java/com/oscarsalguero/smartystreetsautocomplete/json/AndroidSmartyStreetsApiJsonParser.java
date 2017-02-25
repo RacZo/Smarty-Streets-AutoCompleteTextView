d881963eb640a818f44edc5c382bc03e1be7d4e7
Oscar S <oscar@realactionsoft.com>
2017-02-25 16:07:29 -0500

    Added copyright message to all classes

--- src/main/java/com/oscarsalguero/smartystreetsautocomplete/json/AndroidSmartyStreetsApiJsonParser.java
+++ src/main/java/com/oscarsalguero/smartystreetsautocomplete/json/AndroidSmartyStreetsApiJsonParser.java
@@ -1,3 +1,14 @@
+/***
+ * Copyright (c) 2017 Oscar Salguero www.oscarsalguero.com
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.oscarsalguero.smartystreetsautocomplete.json;
 
 import android.util.JsonReader;
@@ -16,6 +27,9 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * JSON parser
+ */
 class AndroidSmartyStreetsApiJsonParser implements SmartyStreetsApiJsonParser {
 
     @Override
